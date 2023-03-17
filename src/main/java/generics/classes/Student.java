60ed77acaf58ff76064990fb6d2f3cfb1ca08a4e
okcuoglu1 <okcuoglucemil@gmail.com>
2023-03-17 15:11:19 +0300

    Pojo Class

--- src/main/java/generics/classes/Student.java
+++ src/main/java/generics/classes/Student.java
@@ -20,6 +20,33 @@ Task: 1) Java dersi için, öğrencileri isim, vize notu ve final notunu  ile bi
     private U u;
     private V v;
 
+    public Student(S s, U u, V v) {
+        this.s = s;
+        this.u = u;
+        this.v = v;
+    }
 
+    public S getS() {
+        return s;
+    }
 
+    public void setS(S s) {
+        this.s = s;
+    }
+
+    public U getU() {
+        return u;
+    }
+
+    public void setU(U u) {
+        this.u = u;
+    }
+
+    public V getV() {
+        return v;
+    }
+
+    public void setV(V v) {
+        this.v = v;
+    }
 }
